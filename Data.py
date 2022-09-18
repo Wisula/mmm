ba83c37a0d20cf2d731def69a05804031f6ecc63
Wisula <99121477+Wisula@users.noreply.github.com>
2022-09-18 10:53:21 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,16 +4,18 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     # Start Message
     START = """
-Hey {}
+❣ ʜᴇʟʟᴏ {} ❣
 
-Welcome to {}
+ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ ꜰᴏʀᴄᴇꜱᴜʙ ʙᴏᴛ 💖
 
-I can force your group's users to join a particular chat. 
-The chat can be a group or channel. It can be private or public.
+ɪ ᴄᴀɴ ꜰᴏʀᴄᴇ ʏᴏᴜʀ ɢʀᴜᴏᴘ'ꜱ ᴜꜱᴇʀꜱ ᴛᴏ ᴊᴏɪɴ ᴀ ᴘᴀʀᴛɪᴄᴜʟᴀʀ ᴄʜᴀᴛ❄️
+ᴛʜᴇ ᴄʜᴀᴛ ᴄᴀɴ ʙᴇ ᴀ ɢʀᴏᴜᴘ ᴏʀ ᴀ ᴄʜᴀɴɴᴇʟ☘
+ɪᴛ ᴄᴀɴ ʙᴇ ᴘʀɪᴠᴀᴛᴇ ᴏʀ ᴘᴜʙʟɪᴄ🍄
 
-Use below buttons to learn more !
+ᴜꜱᴇ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴꜱ ᴛᴏ ʟᴇᴀʀɴ ᴍᴏʀᴇ👻
 
-By @ImDark_Empire
+ᴘᴏᴡᴇʀᴅ ʙʏ @EpicBotsSl 🇱🇰
+
     """
 
     # Home Button
@@ -23,48 +25,48 @@ By @ImDark_Empire
 
     # Rest Buttons
     buttons = [
-        [InlineKeyboardButton("🤖 Bot Status and More Bots 🤖", url="https://t.me/SLBotOfficial/28")],
+        [InlineKeyboardButton("🤖 More Bots 🤖", url="https://t.me/EpicBotsSl")],
         [
             InlineKeyboardButton("❔ How to Use ❔", callback_data="help"),
             InlineKeyboardButton("♾️ About ♾️", callback_data="about")
         ],
-        [InlineKeyboardButton("♥ More Amazing bots ♥", url="https://t.me/SLBotOfficial")],
-        [InlineKeyboardButton("🛫 Support Group 🛫", url="https://t.me/trtechguide")],
+        [InlineKeyboardButton("♻️ Owner ♻️", url="https://t.me/wisula4")],
+        [InlineKeyboardButton("🛫 Support Group 🛫", url="https://t.me/Epicchats")],
     ]
 
     # Help Message
     HELP = """
-1) Add me as **Admin** to a group.
+1) Add me as **Admin** to a group👩‍💻
 
-2) Add me to the particular chat as **Admin** where you want to force your users to join. It can be any group or channel, public or private.
+2) Add me to the particular chat as **Admin** where you want to force your users to join. It can be any group or channel, public or private🤗
 
 3) Use /fsub chat_id/username to make me functional. Use /id if you need chat id.
-Example : `/fsub -1001505616678` or `/forcesubscribe -1001375849192`
+Example : `/fsub --1001620454933` or `/forcesubscribe -1001620454933`
 
-4) [Optional] Use /settings to change settings!
+4) [Optional] Use /settings to change settings! 🙄
 
-5) You are good to go. Leave the rest to me.
+5) You are good Leave the rest to me🥰
 
 🤖 **Available Commands** 🤖
 
-/fsub - See current force subscribe chat
-/fsub chat_id/username - Force users to join the particular chat
-/settings - Change Group Settings
-/id - Get the chat id of any group or channel
-/about - About The Bot
-/help - This Message
-/start - Start the Bot
+/fsub - See current force subscribe chat ☘️
+/fsub chat_id/username - Force users to join the particular chat☘️
+/settings - Change Group Settings☘️
+/id - Get the chat id of any group or channel☘️
+/about - About The Bot☘️
+/help - This Message☘️
+/start - Start the Bot☘️
 
-**Note** - You can also use `/forcesubscribe` instead of `/fsub`
+**Note** - You can also use `/forcesubscribe` instead of `/fsub`😊
     """
 
     # About Message
     ABOUT = """
-**About This Bot** 
+**About This Bot** 🤩
 
-A telegram force subscribing bot by @SLBotOfficial
+A telegram force subscribing bot by @EpicBotsSl 🇱🇰
 
-Source Code : [Click Here](https://github.com/DARKEMPIRESL/ForceSubscribeBot)
+Source Code : [wisula](https://github.com/wisula)
 
 Framework : [Pyrogram](docs.pyrogram.org)
 
